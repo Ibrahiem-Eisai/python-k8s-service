f6d4044616c780ace60345c549bc51f09df19cc0
Ibrahiem-Eisai <Ibrahiem_Mohammad@eisai.com>
2023-02-15 03:53:51 +0000

    fix cicd

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,5 +9,5 @@ def blue_or_green():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=True,host='0.0.0.0',port=port)
